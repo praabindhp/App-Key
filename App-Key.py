77ff4afec67e91f6916ffa50082f263b31d33d19
praabindhp <praabindhp@gmail.com>
2022-11-27 22:02:07 +0530

    Updated

---
+++ App-Key.py
@@ -0,0 +1,71 @@
+# Initializing The Libraries
+import webbrowser
+import keyboard
+import msvcrt
+from sshkeyboard import listen_keyboard
+
+def set_app():
+    print("\nSet The App-Key")
+    print("---------------")
+    print('''The Hotkey Is : "Space"''')
+    print('''Format : Hotkey + "Your Text"''')
+    print('Leave Key-Text Empty To Exit')
+    
+    app_key = {}
+    while True:
+        k = input("\nEnter Key-Text : ")
+        if k == '':
+            print('App-Key Set Successfully :)')
+            break
+        url = input("Enter URL : ")
+        app_key[k] = url
+        global final_key
+        final_key = app_key
+        
+    return final_key
+            
+def start_app():
+    print("Enter Value : ")
+    char = msvcrt.getch()
+    print(char)
+    if 'g' in char:
+        webbrowser.open('https://www.google.com/')
+    elif char == "b'f'":
+        webbrowser.open('https://www.facebook.com/')
+    elif char == "b'y'":
+        webbrowser.open('https://www.youtube.com/')
+
+def stop_app():
+    pass
+  
+if __name__ == "__main__":
+    while True:
+        print("\nWelcome To App-Key Application")
+        print("------------------------------")
+        print("Select Choice Of Execution")
+        print("1 : Set App-Key")
+        print("2 : Start App-Key")
+        print("3 : Stop App-Key")
+        print()
+
+        choice = int(input("Enter The Choice : "))
+        if choice == 1:
+            set_app()
+            print(final_key)
+            continue
+            
+        elif choice == 2:
+            start_app()
+            continue
+            
+        else:
+            exit()
+
+#     if keyboard.is_pressed(hk + '+ g'): 
+#         webbrowser.open('https://www.google.com/')
+        
+#     if keyboard.is_pressed(hk + '+ y'): 
+#         webbrowser.open('https://www.youtube.com/')
+        
+#     if keyboard.is_pressed(hk + '+ f'):
+#         webbrowser.open('https://www.facebook.com/')
